27e9fe6135a4365d9cda18c9067d7a2d2f171a53
Dawid <dawid.marszalek99@gmail.com>
2023-01-11 19:48:25 +0100

    add generated project from Spring Initializr

---
+++ src/main/java/com/example/schoollibrary/SchoolLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoollibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolLibraryApplication.class, args);
+	}
+
+}
